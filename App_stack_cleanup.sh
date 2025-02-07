8c349f642e53727aedf28067f9f62197341a5520
CheezPascal <rascal.chisom@gmail.com>
2025-02-07 13:30:06 -0500

    Updated some lines

--- App_stack_cleanup.sh
+++ App_stack_cleanup.sh
@@ -9,5 +9,9 @@ docker rm mysql_db nginx_server node_app
 # Show remaining Docker resources
 echo "Containers removed. Volumes and network are still available."
 docker ps -a
+echo
+sleep 2
 docker network ls
+echo 
+sleep 2
 docker volume ls
